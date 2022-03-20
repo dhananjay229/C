6363415f3d3a051468c23a2a393bdb0772f5f876
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-03-20 09:12:27 -0700

    Add files via upload

---
+++ 2D_Array/input&ouput.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a[5][3] = {0};
+	
+	int val = 1;
+	
+	for(int row=0; row<=4; row++){
+		for(int col=0; col<=2; col++){
+			a[row][col] = val;
+			val = val+1;
+			cout<< a[row][col] <<" ";
+			
+		}
+		cout<<endl;
+	}
+	
+	
+	
+	
+	
+	return 0;
+} 
