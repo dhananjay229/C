6a0f030f456a4753345dc3631dde4c16633c9b04
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-03-30 06:55:10 -0700

    Add files via upload

---
+++ Recursion/DecreasingRecursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void dec(int n){
+	if( n == 0){
+		return;
+	}
+	else{
+		cout<<n<<" ";
+		dec(n-1);
+	}
+}
+
+int main(){
+	int n;
+	
+	cin>>n;
+	dec(n);
+	return 0;
+}
